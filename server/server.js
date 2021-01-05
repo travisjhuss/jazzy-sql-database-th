b43a8084c12a0f6fb602c444a5a0d43b69748be0
Travis Huss <travisjhuss@gmail.com>
2021-01-05 17:14:20 -0600

    post route with pool set up for /artist

--- server/server.js
+++ server/server.js
@@ -83,8 +83,19 @@ app.get('/artist', (req, res) => {
 });
 
 app.post('/artist', (req, res) => {
-    artistList.push(req.body);
-    res.sendStatus(201);
+    // artistList.push(req.body);
+    // res.sendStatus(201);
+    const queryText = `
+        INSERT INTO "artist" ("name", "birthdate")
+        VALUES ($1, $2);
+    `;
+    pool.query(queryText, [req.body.name, req.body.birthdate])
+        .then((result) => {
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        }); // end pool.query
 });
 
 app.get('/song', (req, res) => {
