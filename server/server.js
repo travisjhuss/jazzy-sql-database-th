90f3768de4534a17b1cc66f0369ebde05cd73a4a
Travis Huss <travisjhuss@gmail.com>
2021-01-05 16:41:53 -0600

    added pool info to server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,26 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+// import pg
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 'jazzy_sql',
+    host: 'localhost',
+    port: '5432'
+}); // end pool
+
+// helper stuff
+pool.on('connect', () => {
+    console.log('PG CONNECTED');
+});
+
+pool.on('error', () => {
+    console.log(error);
+
+});
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
